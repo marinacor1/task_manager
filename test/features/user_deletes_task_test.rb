1b38a8a3971676703149d9cce5037fca98853b64
marinacor1 <marinaspam1234@gmail.com>
2016-03-23 17:56:08 -0600

    removing comments

--- test/features/user_deletes_task_test.rb
+++ test/features/user_deletes_task_test.rb
@@ -7,12 +7,7 @@ class UserDeletesTask < Minitest::Test
   def test_user_can_delete_a_task
     visit '/'
     create_tasks(1)
-      # click_link("New Task")
-      #
-      # assert_equal "/tasks/new", current_path
-      # fill_in 'task[title]', with: "Learn Capybara" #new.erb
-      # fill_in 'task[description]', with: "Its super helpful" #new.erb
-      # click_button("Submit")
+  
     visit '/tasks'
       within(".buttons") do
         assert page.has_content?("task title 1")

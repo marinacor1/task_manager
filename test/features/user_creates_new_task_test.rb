8309c1fca1df345465e751dd333059693b4c21b1
marinacor1 <marinaspam1234@gmail.com>
2016-03-23 16:23:19 -0600

    first features test passing

---
+++ test/features/user_creates_new_task_test.rb
@@ -0,0 +1,19 @@
+require_relative '../test_helper'
+
+class UserCanCreateNewTaskTest < Minitest::Test
+  include TestHelpers
+  include Capybara::DSL
+  
+  def test_user_can_create_new_task
+    visit '/'
+    click_link("New Task")
+    assert_equal "/tasks/new", current_path
+    fill_in 'task[title]', with: "Learn Capybara" #new.erb
+    fill_in 'task[description]', with: "Its super helpful" #new.erb
+    click_button("Submit")
+    assert_equal "/tasks", current_path
+    within(".buttons") do
+      assert page.has_content?("Learn Capybara") #index.erb
+    end
+  end
+end

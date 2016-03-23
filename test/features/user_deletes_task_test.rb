67d1164f59e978a3b04e2059d94acbd69df9f42a
marinacor1 <marinaspam1234@gmail.com>
2016-03-23 17:55:57 -0600

    using test helper

--- test/features/user_deletes_task_test.rb
+++ test/features/user_deletes_task_test.rb
@@ -6,14 +6,14 @@ class UserDeletesTask < Minitest::Test
 
   def test_user_can_delete_a_task
     visit '/'
-      task_manager.create(1)
+    create_tasks(1)
       # click_link("New Task")
       #
       # assert_equal "/tasks/new", current_path
       # fill_in 'task[title]', with: "Learn Capybara" #new.erb
       # fill_in 'task[description]', with: "Its super helpful" #new.erb
       # click_button("Submit")
-
+    visit '/tasks'
       within(".buttons") do
         assert page.has_content?("task title 1")
       end
